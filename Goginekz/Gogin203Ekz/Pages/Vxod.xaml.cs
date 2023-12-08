2470bc25741471df7eb4c096e4a25baadd8632b7
gruppa203 <149385686+gruppa203@users.noreply.github.com>
2023-12-08 12:11:37 +0300

    1

---
+++ Goginekz/Gogin203Ekz/Pages/Vxod.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Gogin203Ekz.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Vxod.xaml
+    /// </summary>
+    public partial class Vxod : Page
+    {
+        public Vxod()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var userObj = BdGoginEntities2.getcontect().Polzov.FirstOrDefault(x => x.Pochta== Log.Text && x.Parol == Par.Text);
+            if (userObj == null)
+            {
+                MessageBox.Show("Пользователь с таким логином и паролем!", "Ошибка входа", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else
+            {
+                MessageBox.Show("Здраствуйте  " + userObj.Familia + "!", "Уведомление", MessageBoxButton.OK, MessageBoxImage.Information);
+                this.NavigationService.Navigate(new tablic());
+            }
+            this.NavigationService.Navigate(new tablic());
+        }
+    }
+}
---
+++ Goginekz/Gogin203Ekz/Pages/tablic.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Gogin203Ekz.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для tablic.xaml
+    /// </summary>
+    public partial class tablic : Page
+    {
+        public tablic()
+        {
+            InitializeComponent();
+            Tablic.ItemsSource = BdGoginEntities2.getcontect().Naim_Opis.ToList();
+           
+        }
+
+        private void tabl_1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+          
+        }
+
+        private void Tablic_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
